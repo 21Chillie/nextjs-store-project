c42b01bcd20588977258cb1259698a16d7b5f18d
Chillie <reskialamsyah@gmail.com>
2026-07-23 15:34:43 +0800

    feat(db): add cart and review models and fix schema typos

--- types/global.type.ts
+++ types/global.type.ts
@@ -1,3 +1,5 @@
+import { CartItem, Product } from "@/lib/generated/prisma/client";
+
 export type SearchProps = {
   searchParams: Promise<{
     query?: string;
@@ -10,4 +12,12 @@ export type ProductServerResponse = {
   message?: string;
   errors?: Record<string, string[]>;
   inputs?: Record<string, string>;
+};
+
+export type CartItemWithProductPrice = CartItem & {
+  product: Pick<Product, "price">;
+};
+
+export type CartItemWithProduct = CartItem & {
+  product: Pick<Product, "id" | "name" | "image" | "company" | "price">;
 };
