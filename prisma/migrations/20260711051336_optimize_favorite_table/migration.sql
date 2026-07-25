6967ef10ead014a453093fe2470c7493955efd31
Chillie <reskialamsyah@gmail.com>
2026-07-25 14:03:11 +0800

    Delete migration.sql

--- prisma/migrations/20260711051336_optimize_favorite_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[clerkId,productId]` on the table `Favorite` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Favorite_clerkId_productId_key" ON "Favorite"("clerkId", "productId");
