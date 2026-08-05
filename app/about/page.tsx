3405d94a58419f53276dcee654ca5164f25e2664
Chillie <reskialamsyah@gmail.com>
2026-08-06 00:07:56 +0800

    feat: add about page sections (company, store, mission, sustainability)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,15 @@
+import AboutCompany from "@/components/about/AboutCompany";
+import Mission from "@/components/about/Mission";
+import OurStore from "@/components/about/OurStore";
+import Sustainability from "@/components/about/Sustainability";
+
 export default async function About() {
-  return <div className="space-y-2">About Page</div>;
+  return (
+    <div className="flex flex-col gap-12">
+      <AboutCompany />
+      <OurStore />
+      <Mission />
+      <Sustainability />
+    </div>
+  );
 }
