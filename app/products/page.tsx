2c508fb9e2c1a99869a8730552967ccaada30710
Chillie <reskialamsyah@gmail.com>
2026-08-06 00:08:08 +0800

    fix: use Loading component in products Suspense fallback

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,11 @@
+import Loading from "@/components/global/loading";
 import ProductsContainer from "@/components/products/products-container";
 import { SearchProps } from "@/types/global.type";
 import { Suspense } from "react";
 
 export default async function Products({ searchParams }: SearchProps) {
   return (
-    <Suspense fallback={<div>loading...</div>}>
+    <Suspense fallback={<Loading />}>
       <ProductsContainer searchParams={searchParams} />
     </Suspense>
   );
