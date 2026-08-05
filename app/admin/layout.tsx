ef39a5e04e9fecb3ba6154c1bc312ad1e0019945
Chillie <reskialamsyah@gmail.com>
2026-08-06 00:08:03 +0800

    refactor: split admin layout into async auth + sync Suspense wrapper

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,12 +1,15 @@
+import Loading from "@/components/global/loading";
 import SectionTitle from "@/components/global/section-title";
 import SidebarAdmin from "@/components/global/sidebar-admin";
+import { protectAdminRoute } from "@/lib/protect-route";
 import { Suspense } from "react";
 
-export default function AdminLayout({
+async function ProtectedAdminContent({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  await protectAdminRoute();
   return (
     <section id="section-admin">
       <header>
@@ -14,7 +17,7 @@ export default function AdminLayout({
       </header>
 
       <div className="grid grid-cols-1 md:grid-cols-12 md:border-x md:border-b">
-        <div className="w-full border border-t-0 p-4 md:col-span-2 md:flex md:items-start md:justify-center md:border-0">
+        <div className="w-full border border-t-0 p-4 md:col-span-2 md:flex md:items-start md:justify-center md:border-0 max-sm mb-4">
           <Suspense>
             <SidebarAdmin />
           </Suspense>
@@ -23,5 +26,17 @@ export default function AdminLayout({
         <div className="md:col-span-10 md:border-l md:p-4">{children}</div>
       </div>
     </section>
+  );
+}
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Suspense fallback={<Loading />}>
+      <ProtectedAdminContent>{children}</ProtectedAdminContent>
+    </Suspense>
   );
 }
